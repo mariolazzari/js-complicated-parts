7b5a07d835d11bea341905bc1f22fb582586a48f
Mario <mario.lazzari@gmail.com>
2020-08-24 21:54:16 +0200

    Done lesson 1

--- 01_prototypes.js
+++ 01_prototypes.js
@@ -1,1 +1,22 @@
 // Prototypes
+let car1 = {
+  model: "Ford",
+  year: 2020,
+  color: "blue",
+};
+
+function carDescription() {
+  return `This is a ${this.model}.`;
+}
+
+Object.prototype.carDescription = carDescription;
+
+// constructor
+function Vehicle(model, year, color) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+}
+
+let car2 = new Vehicle("Toyota", 2019, "red");
+let car3 = Object.create(car2);
