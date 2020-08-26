f7488445a54f13fa1a51f6b265f8a5b6423e6b72
Mario Lazzari <mario.lazzari@gmail.com>
2020-08-26 19:47:06 +0200

    Done lesson 2

--- 02_this.js
+++ 02_this.js
@@ -6,7 +6,23 @@ const moscow = {
 
   logMuseums: function () {
     console.log(this === moscow);
+    console.log(`In Moscow there are ${this.museums} museums.`);
   },
 };
 
 moscow.logMuseums();
+
+// constructor
+function City(city, country) {
+  this.city = city;
+  this.country = country;
+}
+
+const riva = new City("Riva del Garda", "Italia");
+console.log(riva);
+
+// method
+City.prototype.info = function () {
+  console.log(`${this.city} is in ${this.country}.`);
+};
+riva.info();
